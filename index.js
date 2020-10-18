b31459da02eaf94d52e388f783696c352b9b87f8
guillenjs <jorgeguilln@gmail.com>
2020-10-18 12:23:08 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
-
+    let minArr = array[0]
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++) {
+        let currentElement = array[i]
+        if (array[i] < minArr) 
+            {
+                minArr = array[i];
+                minIndex = i
+            }
+    }
+    array.splice(minIndex, 1);
+    return minArr;
 }
 
 function selectionSort(array){
-
+    let newMin;
+    let sorted = [];
+    while (array.length != 0) {
+      newMin = findMinAndRemove(array);
+      sorted.push(newMin);
+    }
+    return sorted;
 }
